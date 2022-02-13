8261d28c4e0280f024a728a1349fb0e373b329b6
Anthony <tonysim01@hotmail.com>
2022-02-13 18:05:48 +0100

    addAllProjects

--- projects.php
+++ projects.php
@@ -9,14 +9,36 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="projectsstyle.css">
+    <title>Tous mes projets</title>
 </head>
 <body>
+<a href="admin.php">Retour à l'admin</a>
+<a href="index.php">Retour au portfolio</a>
+<h1>Mes Projets</h1>
+
+<div id="allprojects">
+    <div class="project">
+        <div class="projectimg"></div>
+        <div class="buttonproject">
+            <button>Modifier</button>
+            <button>Supprimer</button>
+        </div>
+    </div>
+    <div class="project">
+        <div class="projectimg"></div>
+        <div class="buttonproject">
+            <button>Modifier</button>
+            <button>Supprimer</button>
+        </div>
+    </div>
+</div>
+
     
 </body>
 </html>
