3dce8f099abc2502924f4287cb46e5fb4afbeac8
Anthony <tonysim01@hotmail.com>
2022-03-13 16:58:25 +0100

    ModifProject

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta description="Site Web  - Portfolio SIMONNEAU Anthony">
     <link rel="stylesheet" href="style.css" />
     <title>MON PORTFOLIO</title>
   </head>
@@ -23,7 +24,7 @@
               <ul>
                 <li><a href="#about"  class="linkburger" data-after="A Propos">A Propos</a></li>
                 <li><a href="#projets" class="linkburger" data-after="Projets">Mes Projets</a></li>
-                <li><a href="#parcours" class="linkburger" data-after="Parcours">Mon Parcours</a></li>
+                <!-- <li><a href="#parcours" class="linkburger" data-after="Parcours">Mon Parcours</a></li> -->
                 <li><a href="#contact" class="linkburger" data-after="Contact">Contact</a></li>
 
               </ul>
@@ -48,11 +49,11 @@
         </div>
     </div>
     <div class="social">
-      <a href="https://github.com/Anthony-S58" target="_blank"><i class="fab fa-github"></i></a>
-      <a href="https://www.linkedin.com/in/anthony-simonneau-5545b8214/" target="_blank"><i class="fa fa-linkedin"></i></a>
-      <a href="#"><i class="fa fa-slack"></i></a>
-      <a href="#"><i class="fab fa-discord"></i></a>
-      <a href="#"><i class="fab fa-gitkraken"></i></a>
+      <a href="https://github.com/Anthony-S58" target="_blank"><i class="fab fa-github" title="Github" alt="github"></i></a>
+      <a href="https://www.linkedin.com/in/anthony-simonneau-5545b8214/" target="_blank"><i class="fa fa-linkedin" title="Linkedin" alt="linkedin"></i></a>
+      <a href="#"><i class="fa fa-slack" title="Slack" alt="slack"></i></a>
+      <a href="#"><i class="fab fa-discord" title="Discord" alt="discord"></i></a>
+      <a href="#"><i class="fab fa-gitkraken" title="Gitkraken" alt="gitkraken"></i></a>
     </div>
     </section>
 
@@ -68,31 +69,26 @@
           </div>
 
           <div class="texte">
-
-            <p>
-              En formation à l'Acces Code School de Nevers(58) depuis Mars 2021.
-              Profil tertiaire et polyvalent, j'ai travaillé dans beaucoup de domaines.
-              </p>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non laboriosam suscipit soluta odio, explicabo ad distinctio consequatur fugit ipsam, id eaque. Aperiam est architecto commodi!</p>
               <p>
-            
-            Fan d'informatique, de jeux vidéos et de nouvelles technologies.
-              J'ai décidé de me consacrer au code en janvier 2021 et je découvre chaque jour, un vaste univers qui me passionne de plus en plus...
-            </p>
+                Passionné d'informatique, de jeux vidéos et de nouvelles technologies,
+                j'ai décidé de me consacrer au code en janvier 2021 et faire une reconversion professionnelle à l'Access Code School.
+              </p>
+              <p>Mon titre en poche, je continue de me perfectionner via divers projets et langages.</p>
+              <p>J'ai décidé d'intégrer l'école Microsoft pour être Développeur en Intelligence Artificielle.</p>
           </div>
         </div>
       <!-- MES SKILLS -->
       <div class="skills">
         
-        <i class="fab fa-html5" title="Html"></i>
-        <i class="fab fa-css3-alt" title="Css"></i>
-        <i class="fab fa-less" title="Less"></i>
-        <i class="fab fa-sass" title="Sass"></i>
-        <i class="fab fa-js" title="Javascript"></i>
-        <i class="fab fa-php" title="Php"></i>
-        <i class="fab fa-symfony" title="Symfony"></i>
-        <i class="fab fa-python" title="Python"></i>
-        <i class="fab fa-figma" title="Figma"></i>
+        <i class="fab fa-html5" title="Html" alt="html"></i>
+        <i class="fab fa-css3-alt" title="Css" alt="css"></i>
+        <i class="fab fa-less" title="Less" alt="less"></i>
+        <i class="fab fa-sass" title="Sass" alt="sass"></i>
+        <i class="fab fa-js" title="Javascript" alt="javascript"></i>
+        <i class="fab fa-php" title="Php" alt="php"></i>
+        <i class="fab fa-symfony" title="Symfony" alt="symfony"></i>
+        <i class="fab fa-python" title="Python" alt="python"></i>
+        <i class="fab fa-figma" title="Figma" alt="figma"></i>
       </div>
     </section>
 
@@ -118,11 +114,11 @@
                             <?php
                             if( !empty($donnees['image'])){
                             ?>
-                                <img class="imageview" src="uploads/<?=$donnees['image']?>" width="auto" height="auto" alt="" title="Voir projet <?=$donnees['title']?>">
+                                <img class="imageview" src="uploads/<?=$donnees['image']?>" width="auto" height="auto" alt="<?=$donnees['title']?>" title="Voir projet <?=$donnees['title']?>">
                             <?php
                             }else{
                                 ?>
-                                <img class="imageview" src="IMG/pasdimage.png" width="auto" height="auto" alt="" title="Voir projet <?=$donnees['title']?>"> 
+                                <img class="imageview" src="IMG/pasdimage.png" width="auto" height="auto" alt="<?=$donnees['title']?>" title="Voir projet <?=$donnees['title']?>"> 
                             <?php
                             };
                             ?>
@@ -137,22 +133,22 @@
                             <div id="projetlanguage">
                                 <?php
                                     if ($donnees['html']=='oui'){
-                                        echo '<br><img src="IMG/icons/htmlicon.png" alt="" width="30px" height="30px"><br>';
+                                        echo '<br><img src="IMG/icons/htmlicon.png" alt="html icon" width="30px" height="30px" title="HTML"><br>';
                                     }else{};
                                     if ($donnees['css']=='oui'){
-                                        echo '<br><img src="IMG/icons/cssicon.png" alt="" width="30px" height="30px"><br>';
+                                        echo '<br><img src="IMG/icons/cssicon.png" alt="css icon" width="30px" height="30px" title="CSS"><br>';
                                     }else{};
                                     if ($donnees['javascript']=='oui'){
-                                        echo '<br><img src="IMG/icons/jsicon.png" alt="" width="30px" height="30px"><br>';
+                                        echo '<br><img src="IMG/icons/jsicon.png" alt="javascript icon" width="30px" height="30px" title="Javascript"><br>';
                                     }else{};
                                     if ($donnees['php']=='oui'){
-                                        echo '<br><img src="IMG/icons/phpicon.png" alt="" width="30px" height="30px"><br>';
+                                        echo '<br><img src="IMG/icons/phpicon.png" alt="php icon" width="30px" height="30px" title="PHP"><br>';
                                     }else{};
                                     if ($donnees['mysql']=='oui'){
-                                        echo '<br><img src="IMG/icons/mysqlicon.png" alt="" width="30px" height="30px"><br>';
+                                        echo '<br><img src="IMG/icons/mysqlicon.png" alt="mysql icon" width="30px" height="30px" title="MySQL"><br>';
                                     }else{};
                                     if ($donnees['symfony']=='oui'){
-                                        echo '<br><img src="IMG/icons/symfonyicon.png" alt="" width="30px" height="30px"><br>';
+                                        echo '<br><img src="IMG/icons/symfonyicon.png" alt="symfony icon" width="30px" height="30px" title="Symfony"><br>';
                                     }else{};
                                 ?>
                             </div>
